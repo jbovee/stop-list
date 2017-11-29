deccf7e25fb7cd70fd2631d109b1c6b154a91777
jbovee <Mitouch.b@gmail.com>
2017-11-29 01:33:52 -0500

    Changed to eventPage isntead of background page. Functionality implemented for two types of playlist urls

---
+++ eventPage.js
@@ -0,0 +1,35 @@
+var menuItem = {
+	"id": "testing",
+	"title": "testing",
+	"contexts": ["link"],
+	"targetUrlPatterns": ["*://*.youtube.com/*&list=*", "*://*.youtube.com/*&video_ids=*"]
+}
+chrome.contextMenus.create(menuItem);
+
+chrome.contextMenus.onClicked.addListener(function(clickData) {
+	var newUrl = getUrlParams(clickData.linkUrl, craftUrl);
+	chrome.tabs.create({"url": newUrl});
+});
+
+function getUrlParams(url, callback) {
+	var extra = url.split('?')[1],
+		args = extra.split('&'),
+		argsJSON = {};
+	args.forEach(function(arg) {
+		var key = arg.split('=')[0],
+			value = (key == "video_ids") ? arg.split('=')[1].split('%2C') : arg.split('=')[1];
+		argsJSON[key] = value;
+	});
+	return callback(argsJSON);
+}
+
+function craftUrl(paramsJson) {
+	var finalURL = "https://www.youtube.com/watch?v=";
+	if (paramsJson["video_ids"]) {
+		var index = (paramsJson["index"]) ? paramsJson["index"] : 0;
+		finalURL += paramsJson["video_ids"][index];
+	} else if (paramsJson["list"]) {
+		finalURL += paramsJson["v"];
+	}
+	return finalURL;
+}
